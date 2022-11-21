04b15a19c3e5dcb6958f54c8dc7cf35e14ea7585
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2022-11-21 23:53:11 +0700

    feat: implement secure store to store role

--- contexts/AuthContext.jsx
+++ contexts/AuthContext.jsx
@@ -1,9 +1,23 @@
-import { createContext, useState } from "react";
+import * as SecureStorage from "expo-secure-store";
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext(undefined);
 
 export const AuthContextProvider = (props) => {
   const [role, setRole] = useState(null);
+
+  const setRoleOnSecureStorage = (role) => {
+    setRole(role);
+    SecureStorage.setItemAsync("role", role);
+  };
+
+  useEffect(() => {
+    const getRoleFromSecureStorage = async () => {
+      const role = await SecureStorage.getItemAsync("role");
+      setRole(role);
+    };
+    getRoleFromSecureStorage();
+  }, []);
 
   const AuthContextObject = {
     role,
